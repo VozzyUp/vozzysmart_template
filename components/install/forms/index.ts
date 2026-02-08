cd78d900767c131b68b9a89b2513a95297620247
VozzyUp <contato@vozzyup.com.br>
2026-02-08 12:53:27 -0300

    Delete components/install directory

--- components/install/forms/index.ts
+++
@@ -1,6 +0,0 @@
-export { IdentityForm } from './IdentityForm';
-export { VercelForm } from './VercelForm';
-export { SupabaseForm } from './SupabaseForm';
-export { QStashForm } from './QStashForm';
-export { RedisForm } from './RedisForm';
-export type { FormProps } from './types';
--- components/install/steps/index.ts
+++
@@ -1,5 +0,0 @@
-export { IdentityStep } from './IdentityStep';
-export { VercelStep } from './VercelStep';
-export { SupabaseStep } from './SupabaseStep';
-export { QStashStep } from './QStashStep';
-export { RedisStep } from './RedisStep';
